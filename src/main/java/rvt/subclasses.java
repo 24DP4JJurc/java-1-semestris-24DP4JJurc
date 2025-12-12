49e1e1957fd3ebe884300fabd61aff14ad31bedd
24DP4JJurc <A240102JJ@rvt.lv>
2025-12-12 10:10:12 +0200

    person and subclasses

---
+++ src/main/java/rvt/subclasses.java
@@ -0,0 +1,72 @@
+package rvt;
+
+import java.util.ArrayList;
+
+public class subclasses {
+
+        public static void main(String[] args) {
+        ArrayList<Person> persons = new ArrayList<Person>();
+        persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
+        persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
+
+        printPersons(persons);
+    }
+
+    public static void printPersons(ArrayList<Person> persons) {
+        for (Person p : persons) {
+            System.out.println(p);
+        }
+    }
+}
+
+class Person {
+    private String name;
+    private String address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String toString() {
+        return this.name + "\n  " + this.address;
+    }
+}
+
+class Student extends Person {
+    private int credits;
+
+    public Student(String name, String address) {
+        super(name, address);
+        this.credits = 0;
+    }
+
+    public int credits() {
+        return this.credits;
+    }
+
+    public void study() {
+        this.credits++;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\n  Study credits " + this.credits;
+    }
+}
+
+class Teacher extends Person {
+    private int salary;
+
+    public Teacher(String name, String address, int salary) {
+        super(name, address);
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\n  salary " + this.salary + " euro/month";
+    }
+}
+
+
